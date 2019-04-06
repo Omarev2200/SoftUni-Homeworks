320c404a2b3680347439937d070d58f04aeb3b40
Stanislav Omarev <stanislav.omarev@gmail.com>
2019-04-06 21:42:21 +0300

    Added programs

---
+++ Technology-FundamentalsC#/DEMO-MID-EXAM/Dictionary/Dictionary/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new Dictionary<string, List<string>>();
+           
+            string[] pairsOfWordsAndDescriptions = Console.ReadLine().Split(" | ").ToArray();
+
+            string command = string.Empty;
+
+            for (int i = 0; i < pairsOfWordsAndDescriptions.Length; i++)
+            {
+                string[] wordDefinition = pairsOfWordsAndDescriptions[i].Split(": ");
+                string word = wordDefinition[0];
+                string definition = wordDefinition[1];
+                if (!dictionary.ContainsKey(word))
+                {
+                    dictionary[word] = new List<string>();
+                }
+                dictionary[word].Add(definition);
+            }
+            while ((command = Console.ReadLine()) != "End")
+            {
+                if (command == "List")
+                {
+                    foreach (var kvp in dictionary.OrderBy(x=>x.Key))
+                    {
+                        Console.Write(kvp.Key+" ");
+                    }
+                    
+                    break;
+                }
+                string[] word = command.Split(" | ");
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (dictionary.ContainsKey(word[i]))
+                    {
+                        Console.WriteLine(word[i]);
+                        foreach (var definition in dictionary[word[i]].OrderByDescending(x=>x.Length))
+                        {
+                            Console.WriteLine($" -{definition}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Technology-FundamentalsC#/DEMO-MID-EXAM/LastStop/LastStop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace LastStop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbersOfThePaintings = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string commands = string.Empty;
+            foreach (var item in numbersOfThePaintings)
+            {
+                Console.WriteLine(item);
+            }
+
+            while ((commands=Console.ReadLine()) !="END")
+            {
+                string[] command = commands.Split().ToArray();
+            }
+        }
+    }
+}
