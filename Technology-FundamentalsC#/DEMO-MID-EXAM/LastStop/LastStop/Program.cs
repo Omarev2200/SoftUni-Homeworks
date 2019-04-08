f6d395448899495417cbc203f5af138ac26711b8
Stanislav Omarev <stanislav.omarev@gmail.com>
2019-04-08 14:29:45 +0300

    Added program

--- Technology-FundamentalsC#/DEMO-MID-EXAM/LastStop/LastStop/Program.cs
+++ Technology-FundamentalsC#/DEMO-MID-EXAM/LastStop/LastStop/Program.cs
@@ -13,15 +13,62 @@ namespace LastStop
                 .ToList();
 
             string commands = string.Empty;
-            foreach (var item in numbersOfThePaintings)
-            {
-                Console.WriteLine(item);
-            }
-
+           
             while ((commands=Console.ReadLine()) !="END")
             {
                 string[] command = commands.Split().ToArray();
+
+                if (command[0]== "Change")
+                {
+                    int paintingNumber = int.Parse(command[1]);
+                    int changedNumber= int.Parse(command[2]);
+                    if (numbersOfThePaintings.Contains(paintingNumber))
+                    {
+                        int index = numbersOfThePaintings.IndexOf(paintingNumber);
+                        numbersOfThePaintings[index] = changedNumber;
+                    }
+                }
+                if (command[0]== "Hide")
+                {
+                    int paintingNumber = int.Parse(command[1]);
+                    if (numbersOfThePaintings.Contains(paintingNumber))
+                    {
+                        numbersOfThePaintings.Remove(paintingNumber);
+                    }
+                }
+                if (command[0] == "Switch")
+                {
+                    int paintingNumber = int.Parse(command[1]);
+                    int paintingNumber2 = int.Parse(command[2]);
+                    if (numbersOfThePaintings.Contains(paintingNumber) && numbersOfThePaintings.Contains(paintingNumber2))
+                    {
+                        int index1 = numbersOfThePaintings.IndexOf(paintingNumber);
+                        int index2 = numbersOfThePaintings.IndexOf(paintingNumber2);
+
+
+                        numbersOfThePaintings[index1] = paintingNumber2;
+                        numbersOfThePaintings[index2] = paintingNumber;
+
+                    }
+                }
+                if (command[0] == "Insert")
+                {
+                    int place = int.Parse(command[1]);
+                    int paintingNumber = int.Parse(command[2]);
+                    
+                    if ((numbersOfThePaintings.Count >= place+1 ) && (place+1>= 0))
+                    {
+
+                        numbersOfThePaintings.Insert(place+1, paintingNumber);
+                    }
+                }
+                if (command[0] == "Reverse")
+                {
+                    
+                    numbersOfThePaintings.Reverse();
+                }
             }
+            Console.WriteLine(String.Join(" ",numbersOfThePaintings));
         }
     }
 }
