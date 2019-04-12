90bbcb08af4b992f6762a19ef0b0680d7948c787
Stanislav Omarev <stanislav.omarev@gmail.com>
2019-04-12 16:15:16 +0300

    Added programs

---
+++ Technology-FundamentalsC#/DEMO-FINAL-EXAM/Activation Keys/Activation Keys/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Activation_Keys
+{
+    class Program
+    {
+
+
+        public static object StrigBild { get; private set; }
+
+        static void Main(string[] args)
+        {
+            string[] keysOfTheGames = Console.ReadLine().Split("&", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            string gameCode = string.Empty;
+
+            var sb = new List<string>();
+
+            for (int i = 0; i < keysOfTheGames.Length; i++)
+            {
+                int lent = keysOfTheGames[i].Length;
+                string code = keysOfTheGames[i];
+
+                if (lent == 25)
+                {
+                    for (int j = 0; j < code.Length; j++)
+                    {
+                        char symbol = code[j];
+                        if (char.IsDigit(symbol))
+                        {
+                            int digit = 9 - int.Parse(symbol.ToString());
+                            gameCode = gameCode + digit;
+                        }
+                        else
+                        {
+                            if (char.IsLetter(symbol))
+                            {
+                                gameCode = gameCode + symbol;
+                            }
+                        }
+                        if (j == 4 || j == 9 || j == 14 || j == 19)
+                        {
+                            gameCode = gameCode + '-';
+                        }
+                    }
+                    sb.Add(gameCode.ToUpper());
+
+                }
+                if (lent == 16)
+                {
+                    for (int j = 0; j < code.Length; j++)
+                    {
+                        char symbol = code[j];
+                        if (char.IsDigit(symbol))
+                        {
+                            int digit = 9 - int.Parse(symbol.ToString());
+                            gameCode = gameCode + digit;
+                        }
+                        else
+                        {
+                            if (char.IsLetter(symbol))
+                            {
+                                gameCode = gameCode + symbol;
+                            }
+
+                        }
+                        if (j == 3 || j == 7 || j == 11)
+                        {
+                            gameCode = gameCode + '-';
+
+                        }
+                    }
+                    sb.Add(gameCode.ToUpper());
+                }
+
+                gameCode = string.Empty;
+            }
+            Console.WriteLine(string.Join(", ", sb));
+        }
+    }
+}
---
+++ Technology-FundamentalsC#/DEMO-FINAL-EXAM/VaporWinterSale/VaporWinterSale/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            var gamePrices = new Dictionary<string, double>();
+            var gameDLC = new Dictionary<string, string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].Contains('-'))
+                {
+                    string[] gamePrice = input[i].Split('-', StringSplitOptions.RemoveEmptyEntries);
+                    string game = gamePrice[0];
+                    double price = double.Parse(gamePrice[1]);
+                    if (gamePrices.ContainsKey(game) == false)
+                    {
+                        gamePrices[game] = 0.00;
+
+                    }
+                    gamePrices[game] = price;
+                }
+                if (input[i].Contains(':'))
+                {
+                    string[] gameUp = input[i].Split(':', StringSplitOptions.RemoveEmptyEntries);
+                    string games = gameUp[0];
+                    string dlc = gameUp[1];
+                    if (gamePrices.ContainsKey(games))
+                    {
+                        gameDLC[games] = dlc;
+                        gamePrices[games] *= 1.20;
+
+                    }
+                }
+            }
+            Dictionary<string, double> gamesReducedPrise = new Dictionary<string, double>();
+            foreach (var game in gamePrices)
+            {
+                if (gameDLC.ContainsKey(game.Key))
+                {
+                    gamesReducedPrise[game.Key] = game.Value * 0.5;
+                }
+                else
+                {
+                    gamesReducedPrise[game.Key] = game.Value * 0.8;
+                }
+            }
+
+            foreach (var game in gamesReducedPrise.OrderBy(x => x.Value))
+            {
+                if (gameDLC.ContainsKey(game.Key))
+                {
+                    Console.WriteLine($"{game.Key} - {gameDLC[game.Key]} - {game.Value:F2}");
+                }
+
+            }
+
+            foreach (var game in gamesReducedPrise.OrderByDescending(x => x.Value))
+            {
+                if (!gameDLC.ContainsKey(game.Key))
+                {
+                    Console.WriteLine($"{game.Key} - {game.Value:F2}");
+                }
+            }
+        }
+    }
+}
+
+}
+    }
+}
